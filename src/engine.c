78e86ff21cb407e528caeffa648fa6e356919df0
Daniel Melani <daniel.melani@gmail.com>
2014-04-18 21:35:15 +0200

    Exit on ESC

--- src/engine.c
+++ src/engine.c
@@ -2,8 +2,16 @@
 #include "engine.h"
 extern GLFWwindow * w;
 
+static void
+key_cb(GLFWwindow *w, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(w, GL_TRUE);
+}
+
 void
 run() {
+	glfwSetKeyCallback(w, key_cb);
+
 	while (!glfwWindowShouldClose(w)) {
 		glfwPollEvents();
 		glfwSwapBuffers(w);
