db82b4433044df253928bd4fe8f28c3e57d23d4b
Daniel Melani <daniel.melani@gmail.com>
2014-04-21 21:18:31 +0200

    Updated test to parse a given file

--- test/testparser.c
+++ test/testparser.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+FILE *input;
+
+void yyerror(char *message);
+
+#define YY_INPUT(buf, result, max) {				\
+	int c = getc(input);					\
+	result = (EOF == c) ? 0 : (*(buf) = c, 1);		\
+}
+
 #include "objparser.peg.c"
 
 int
-main() {
+main(int argc, const char *argv[]) {
+	if (argc != 2) {
+		printf("Usage: %s <file>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	if ((input = fopen(argv[1], "r")) == NULL) {
+		perror("Failed to open file");
+		exit(EXIT_FAILURE);
+	}
 	while(yyparse());
 	return 0;
 }
