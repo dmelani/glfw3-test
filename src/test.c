4091f098a1c9444fd03d590ff62c13c5411f7339
Daniel Melani <daniel.melani@gmail.com>
2014-04-22 21:21:28 +0200

    Added basic config and parsing with libconfig

--- src/test.c
+++ src/test.c
@@ -4,6 +4,7 @@
 #include "engine.h"
 
 GLFWwindow * w;
+config_t cfg;
 
 static void
 glfw_error_cb(int error, const char *desc) {
@@ -22,21 +23,39 @@ deinit() {
 	printf("Shutting down video... ");
 	glfwTerminate();
 	printf("OK\n");
+
+	config_destroy(&cfg);
 }
 
 static void
 init() {
+	int width, height;
+
+	config_init(&cfg);
+	printf("Loading config... ");
+	if (config_read_file(&cfg, "cfg/test.cfg") == CONFIG_FALSE) {
+		printf("FAILED!\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("OK\n");
+
 	glfwSetErrorCallback(glfw_error_cb);
 	
 	printf("Initializing video... ");
-	if (!glfwInit())
+	if (!glfwInit()) {
+		printf("FAILED!\n");
 		exit(EXIT_FAILURE);
+	}
 	atexit(deinit);
 	printf("OK\n");
 
 	printf("Creating window... ");
-	if ((w = glfwCreateWindow(800, 600, "Derp", NULL, NULL)) == NULL)
+	config_lookup_int(&cfg, "test.window.size.w", &width);
+	config_lookup_int(&cfg, "test.window.size.h", &height);
+	if ((w = glfwCreateWindow(width, height, "Derp", NULL, NULL)) == NULL) {
+		printf("FAILED!\n");
 		exit(EXIT_FAILURE);
+	}
 	printf("OK\n");
 
 	printf("Setting OpenGL context... ");
